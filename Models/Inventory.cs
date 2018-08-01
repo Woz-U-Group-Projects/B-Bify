eae59f34f259df36a2c407187256195dd26ad3da
Edward Shields <edwardshields5@gmail.com>
2018-08-01 17:51:43 -0400

    Scaffolded Db Context

---
+++ Models/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BnBify.Models
+{
+    public partial class Inventory
+    {
+        public Inventory()
+        {
+            Rental = new HashSet<Rental>();
+        }
+
+        public uint InventoryId { get; set; }
+        public ushort FilmId { get; set; }
+        public byte StoreId { get; set; }
+        public DateTime LastUpdate { get; set; }
+
+        public Film Film { get; set; }
+        public Store Store { get; set; }
+        public ICollection<Rental> Rental { get; set; }
+    }
+}
