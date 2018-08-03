697cdd35f1c7dfa5ed280ab28b7a050c980d29bc
Edward Shields <edwardshields5@gmail.com>
2018-08-03 18:06:55 -0400

    React JSON UI initial build
    Not blowing up, but not displaying yet

--- bnbifyfront/src/index.js
+++ bnbifyfront/src/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Going to insert 'ContentFeed'
+class ContentFeed extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'items': []
+        }
+    }
+    componentDidMount() {
+        this.getItems();
+    }
+
+    getItems() {
+        fetch('http://localhost:5000/api/values')
+        .then(results => results.json())
+        .then(results => this.setState({'items': results}));
+    }
+    render() {
+        return (
+            <ul>
+                {this.state.items.map(function(item, index) {
+                    return (
+                        <div key={index}>
+                            <h1>{item.title}</h1>
+                            <p>{item.description}</p>
+                        </div>
+                    )
+                }
+                )}
+            </ul>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<ContentFeed />, document.getElementById('root'));
 registerServiceWorker();
