fa54d8ca37a5ac57e6b59bccb472aeb15d83d00a
Edward Shields <edwardshields5@gmail.com>
2018-09-13 12:46:17 -0400

    GitKraken Auto Committ

---
+++ Models/HR_SquaredContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BnBify.Models
+{
+    public partial class HR_SquaredContext : DbContext
+    {
+        public HR_SquaredContext()
+        {
+        }
+
+        public HR_SquaredContext(DbContextOptions<HR_SquaredContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblEmployee> TblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-2K3OM4R\\SQLEXPRESS;Database=HR_Squared;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId);
+
+                entity.ToTable("tblEmployee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
